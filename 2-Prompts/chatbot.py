be3f0d2657baf89c6bbe3c468a0648359d7f7e45
somanath <somanathdiksangi@gmail.com>
2025-06-11 12:57:46 +0530

    Update chatbot.py

--- 2-Prompts/chatbot.py
+++ 2-Prompts/chatbot.py
@@ -1,9 +1,16 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessages ,AIMessages ,SystemMessages
+
 load_dotenv()
 
 model=model=ChatGroq(model="llama3-8b-8192",temperature=1.5)
 chat_hist=[]
+messages=[
+SystemMessages(content="you are good research tool act like that "),
+HumanMessages(content="hiii"),
+AIMessages(content="hello how can I help you today ")
+]
 
 while True:
     user_input=input("you :")
